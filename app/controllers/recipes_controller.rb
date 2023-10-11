4831ea616f3930e7b2d4d3c25b6d0b35aee29464
Mark Michaelis <mallenm1993@gmail.com>
2023-10-11 12:00:08 -0400

    Add index, new, create methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,24 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :type, :culture, :prep_hours, :prep_minutes, :servings)
+  end
 end
