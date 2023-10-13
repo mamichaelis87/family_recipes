16c56c924c5b0418f7d0135b65ff17955d30bbe4
Mark Michaelis <mallenm1993@gmail.com>
2023-10-12 22:15:51 -0400

    Write add/remove functions for steps and ingredients

--- app/javascript/controllers/ingredients_and_steps_controller.js
+++ app/javascript/controllers/ingredients_and_steps_controller.js
@@ -2,6 +2,48 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="ingredients-and-steps"
 export default class extends Controller {
+  static targets = ["ingredientList", "ingredient", "ingredientTemplate", "stepList", "step", "stepTemplate", "warning"]
+  static values = { ingredients: Number, steps: Number }
+  
   connect() {
+    }
+
+  addIngredient() {
+    const clone = this.ingredientTemplateTarget.content.cloneNode(true)
+    clone.querySelector("div>:nth-child(2)").setAttribute("name",`recipe[ingredients_attributes][${this.ingredientsValue}][name]`)
+    clone.querySelector("div>:nth-child(4)").setAttribute("name",`recipe[ingredients_attributes][${this.ingredientsValue}][measurement_amount]`)
+    clone.querySelector("div>:nth-child(6)").setAttribute("name",`recipe[ingredients_attributes][${this.ingredientsValue}][measurement_type]`)
+    this.ingredientListTarget.appendChild(clone)
+    this.ingredientsValue++
   }
+
+  removeIngredient() {
+    if (this.ingredientsValue > 1) {
+    this.warningTarget.textContent = ""
+    this.ingredientListTarget.removeChild(this.ingredientListTarget.lastElementChild)
+    this.ingredientsValue--
+    }
+    else {
+      this.warningTarget.textContent = "You must have at least one ingredient, silly."
+    }
+  }
+
+  addStep() {
+    const clone = this.stepTemplateTarget.content.cloneNode(true)
+    clone.querySelector("li>:nth-child(2)").setAttribute("name",`recipe[steps_attributes][${this.stepsValue}][instructions]`)
+    this.stepListTarget.appendChild(clone)
+    this.stepsValue++
+  }
+
+  removeStep() {
+    if (this.stepsValue > 1) {
+    this.warningTarget.textContent = ""
+    this.stepListTarget.removeChild(this.stepListTarget.lastElementChild)
+    this.stepsValue--
+    }
+    else {
+      this.warningTarget.textContent = "You must have at least one step, silly."
+    }
+  }
+  
 }
