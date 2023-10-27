93bb60fb911c6ef064c91cd0ecb5c6e91a7cbed9
Mark Michaelis <mallenm1993@gmail.com>
2023-10-27 13:41:29 -0400

    Move recipe display to home page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,12 @@ class RecipesController < ApplicationController
     else
       @recipes = Recipe.all.order(:name)
     end
+    if params[:chosen_recipe]
+      @chosen_recipe = Recipe.find(params[:chosen_recipe])
+    else
+      ids = Recipe.pluck(:id)
+      @chosen_recipe = Recipe.find(ids.sample)
+    end
   end
 
   def show
