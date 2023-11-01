6bc392e32136f77607526646c47132e3894aabb9
Mark Michaelis <mallenm1993@gmail.com>
2023-11-01 13:54:24 -0400

    Add protein column to recipes table

---
+++ db/migrate/20231101175050_add_protein_column.rb
@@ -0,0 +1,5 @@
+class AddProteinColumn < ActiveRecord::Migration[7.0]
+  def change
+    add_column :recipes, :protein, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_182111) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_175050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_182111) do
     t.float "servings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "protein"
   end
 
   create_table "steps", force: :cascade do |t|
