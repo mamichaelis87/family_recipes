30396a17320368153912b739919b5b48e64b4204
Mark Michaelis <mallenm1993@gmail.com>
2023-11-06 13:46:53 -0500

    Add comment model and associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ApplicationRecord
   has_many :steps
   accepts_nested_attributes_for :ingredients, :steps
   belongs_to :user
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_183228) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_184312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "recipe_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
