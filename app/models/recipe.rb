79c62f3dbf10711278dde8b0739d04efba1a0b87
Mark Michaelis <mallenm1993@gmail.com>
2023-11-06 13:36:28 -0500

    Add user/recipe association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :steps
   accepts_nested_attributes_for :ingredients, :steps
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_182707) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_183228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_182707) do
     t.datetime "updated_at", null: false
     t.string "protein"
     t.string "author"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "steps", force: :cascade do |t|
