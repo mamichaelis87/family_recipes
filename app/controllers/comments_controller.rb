f36e44c21923fcc5a5e9d77ff30bacc452383cd4
Mark Michaelis <mallenm1993@gmail.com>
2023-11-14 15:51:52 -0500

    Add create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to recipe_path(@comment.recipe.id)
+    else
+      redirect_to root_path, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :recipe_id)
+  end
 end
