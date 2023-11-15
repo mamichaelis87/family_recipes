fc7ebab6abb9f5e43f22b7ef521e911a90cd7187
Mark Michaelis <mallenm1993@gmail.com>
2023-11-15 17:17:12 -0500

    Add authentication to view recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:show, :new, :edit, :destroy]
 
   def index
     if params[:meal_type]
