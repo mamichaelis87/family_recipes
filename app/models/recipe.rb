838ee63ba098b23d06a951ffbdd84efb2a9186b2
Mark Michaelis <mallenm1993@gmail.com>
2023-12-16 16:18:14 -0500

    Fix display bugs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,10 +8,10 @@ class Recipe < ApplicationRecord
   has_one_attached :image
 
   def image_as_thumbnail
-      imgage.variant(resize_to_limit: [450,450]).processed
+      image.variant(resize_to_limit: [450,450]).processed
   end
 
   def recipe_card_as_thumbnail
-    imgage.variant(resize_to_limit: [450,450]).processed
+    recipe_card.variant(resize_to_limit: [450,450]).processed
 end
 end
